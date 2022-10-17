a58057899686d43f775c446eefbb4b156dbf306d
Ulises <ulises.sandoval@uabc.edu.mx>
2022-10-16 23:31:53 -0700

    Mostrando tienda desde api

---
+++ app/models/guitarras.server.js
@@ -0,0 +1,6 @@
+export async function getGuitarras(){
+    const respuesta = await fetch(
+        `${process.env.API_URL}/guitarras?populate=imagen`
+      );
+      return await respuesta.json();
+}
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,13 +1,37 @@
+import Guitarra from "~/components/guitarra";
+import { getGuitarras } from "~/models/guitarras.server";
+import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/guitarras.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
 export async function loader() {
-  const respuesta = await fetch(
-    `${process.env.API_URL}/guitarras?populate=imagen`
-  );
-  const resultado = await respuesta.json();
-  return {};
+  const guitarras = await getGuitarras();
+  return guitarras.data;
 }
 
 const Tienda = () => {
-  return <div>Tienda</div>;
+  const guitarras = useLoaderData();
+  console.log(guitarras);
+  return (
+    <main className="contenedor">
+      <h2 className="heading">Nuestras Coleccion</h2>
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map((guitarra) => (
+            <Guitarra guitarra={guitarra?.attributes} key={guitarra?.id} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Tienda;
