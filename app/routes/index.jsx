1ad7b117788bb21a2dd9ebd31ff6b61ae59cf3bc
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-19 09:41:14 -0700

    Múltiples Llamados a API's y Mostrar las guitarras en el Inicio

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,36 @@
+import { getGuitarras } from "~/models/guitarras.server";
+import { getPosts } from "~/models/posts.server";
+import { useLoaderData } from "@remix-run/react";
+import Guitarra from "~/components/guitarra";
+export function meta() {}
+
+export function links() {}
+
+export async function loader() {
+  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data,
+  };
+}
+
 const Index = () => {
-  return <div>index</div>;
+  const { guitarras, posts } = useLoaderData();
+  return (
+    <>
+      <main className="contenedor">
+        <h2 className="heading">Nuestras Coleccion</h2>
+        {guitarras?.length && (
+          <div className="guitarras-grid">
+            {guitarras.map((guitarra) => (
+              <Guitarra guitarra={guitarra?.attributes} key={guitarra?.id} />
+            ))}
+          </div>
+        )}
+      </main>
+    </>
+  );
 };
 
 export default Index;
