10cc1d568da6cfb8401b88ec1adcbf48953e64bc
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-19 08:44:44 -0700

    Routing dinamico para el Blog

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -4,3 +4,10 @@ export async function getPosts(){
       );
       return await respuesta.json();
 }
+
+export async function getPost(url){
+  const respuesta = await fetch(
+      `${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`
+    );
+    return await respuesta.json();
+}
--- app/routes/posts/$postUrl.jsx
+++ app/routes/posts/$postUrl.jsx
@@ -1,4 +1,17 @@
-import React from "react";
+import { getPost } from "~/models/posts.server";
+
+export async function loader({ params }) {
+  const { postUrl } = params;
+  const post = await getPost(postUrl);
+  if (post.data.length === 0) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Entrada no encontrada",
+    });
+  }
+
+  return post;
+}
 
 const Post = () => {
   return <div>$postUrl</div>;
