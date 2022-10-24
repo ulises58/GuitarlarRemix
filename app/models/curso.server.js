eca910dea8efb3482ee27a85d7c1edcbdb7c8a9e
Ulises <ulises.sandoval@uabc.edu.mx>
2022-10-23 21:51:13 -0700

    Llamar el Contenido para el Curso y aplicar CSS con jsx

---
+++ app/models/curso.server.js
@@ -0,0 +1,6 @@
+export async function getCurso(){
+    const respuesta = await fetch(
+        `${process.env.API_URL}/curso?populate=imagen`
+      );
+      return await respuesta.json()
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,13 @@
 import { getGuitarras } from "~/models/guitarras.server";
 import { getPosts } from "~/models/posts.server";
 import { useLoaderData } from "@remix-run/react";
+import { getCurso } from "~/models/curso.server";
 import ListadoGuitarras from "~/components/listado-guitarras";
 import ListadoPosts from "~/components/listado-posts";
+import Curso from "~/components/curso";
 import stylesGuitarras from "~/styles/guitarras.css";
 import stylesBlogs from "~/styles/blog.css";
+import stylesCurso from "~/styles/curso.css";
 
 export function meta() {}
 
@@ -18,25 +21,35 @@ export function links() {
       rel: "stylesheet",
       href: stylesBlogs,
     },
+    {
+      rel: "stylesheet",
+      href: stylesCurso,
+    },
   ];
 }
 
 export async function loader() {
-  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+  const [guitarras, posts, curso] = await Promise.all([
+    getGuitarras(),
+    getPosts(),
+    getCurso(),
+  ]);
 
   return {
     guitarras: guitarras.data,
     posts: posts.data,
+    curso: curso.data,
   };
 }
 
 const Index = () => {
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
   return (
     <>
       <main className="contenedor">
         <ListadoGuitarras guitarras={guitarras} />
       </main>
+      <Curso curso={curso.attributes} />
       <section className="contenedor">
         <ListadoPosts posts={posts} />
       </section>
